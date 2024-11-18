625ffa96d2e20877c3f9e468f3bd7033ed62718a
william moreno marcham <williammorenomarcham@gmail.com>
2024-11-18 10:38:43 -0500

    correcion de entity y dtos

--- src/productos/dto/update-producto.dto.ts
+++ src/productos/dto/update-producto.dto.ts
@@ -1,4 +1,35 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductoDto } from './create-producto.dto';
+import { IsDecimal, IsString, MinLength, Min, IsOptional, IsPositive } from 'class-validator';
 
-export class UpdateProductoDto extends PartialType(CreateProductoDto) {}
+export class UpdateProductoDto{
+  
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    nombre?: string;
+  
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    descripcion?: string;
+  
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    idMaterial?: string;
+  
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    idPresentacion?: string;
+  
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    imagen?: string;
+  
+    @IsDecimal()
+    @IsPositive() 
+    @Min(0)
+    @IsOptional()  
+    precio?: number;
+}
